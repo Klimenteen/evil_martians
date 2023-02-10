756dacc97a6b9856ec6936fa22a5de76bb3a885d
klimenteen <kpetruchkin@gmail.com>
2023-02-11 03:05:41 +0700

    init

---
+++ src/components/socials/Socials.tsx
@@ -0,0 +1,21 @@
+import GithubIcon from "../icons/GithubIcon";
+import TelegramIcon from "../icons/TelegramIcon";
+import MartiansIcon from "../icons/MartiansIcon";
+import {Link} from 'react-router-dom'
+
+type SocialLayout = 'vertical' | 'horizontal';
+
+const Socials = ({layout}: {layout: SocialLayout}) => {
+    return (
+        <div className={`socials ${layout}`}>
+           <Link to='https://github.com/Klimenteen'>
+                <GithubIcon/>
+           </Link>
+           <Link to='https://t.me/Kliment_dev'>
+                <TelegramIcon/>
+           </Link>
+        </div>
+    )
+}
+
+export default Socials;
---
+++ src/pages/login/LoginPage.tsx
@@ -0,0 +1,11 @@
+
+import FloatingFormLayout from "../../layouts/FloatingFormLayout";
+import LoginForm from "../../components/from/LoginForm";
+const LoginPage = () => {
+    
+    return <FloatingFormLayout >
+        <LoginForm/>
+    </FloatingFormLayout>
+}
+
+export default LoginPage;
